b31d1c3ad6e11c48534dab912658c709ec4f071a
lgsosa <lg.sosa@alumno.um.edu.ar>
2024-04-30 22:06:29 -0300

    mejores names

--- test_materia.py
+++ test_materia.py
@@ -4,19 +4,19 @@ from materia import Materia, Alumno
 
 class TestMateria(unittest.TestCase):
     def test_materia(self):
-        profesor = Profesor("Jane Smith", "Profesora de Física", "67890")
+        profesor = Profesor("Marianela Fuseneco", "Profesora de Física", "67890")
         materia = Materia("Física Cuántica", [profesor])
         self.assertEqual(materia.obtener_profesores(), [profesor])
 
     def test_alumno(self):
-        alumno = Alumno("Alice Johnson", "A123")
-        self.assertEqual(alumno.obtener_nombre(), "Alice Johnson")
+        alumno = Alumno("Luisito Comunica ", "A123")
+        self.assertEqual(alumno.obtener_nombre(), "Luisito Comunica ")
         self.assertEqual(alumno.obtener_legajo(), "A123")
 
     def test_materia_alumnos(self):
         materia = Materia("Programación Avanzada", [])
-        alumno1 = Alumno("Bob Brown", "B456")
-        alumno2 = Alumno("Carol White", "C789")
+        alumno1 = Alumno("Pepe Argento", "B456")
+        alumno2 = Alumno("Mony Argento", "C789")
         materia.obtener_alumnos().extend([alumno1, alumno2])
         self.assertEqual(materia.obtener_alumnos(), [alumno1, alumno2])
 
--- test_profesor.py
+++ test_profesor.py
@@ -3,11 +3,10 @@ from profesor import Profesor
 
 class TestProfesor(unittest.TestCase):
     def test_profesor(self):
-        profesor = Profesor("John Doe", "Profesor de Matemáticas", "12345")
-        self.assertEqual(profesor.obtener_nombre(), "John Doe")
+        profesor = Profesor("Pepe Cononda", "Profesor de Matemáticas", "12345")
+        self.assertEqual(profesor.obtener_nombre(), "Pepe Cononda")  # Corregido el nombre aquí
         self.assertEqual(profesor.obtener_cargo(), "Profesor de Matemáticas")
         self.assertEqual(profesor.obtener_legajo(), "12345")
-
 
 if __name__ == '__main__':
     unittest.main()
